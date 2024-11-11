f2df2fc7c46d18919e42086667f1e8b06e9aded9
Benjamin Curtain <benjamin.curtain@gmail.com>
2024-11-11 21:53:30 +1100

    Initial commit

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface BasicProps {
+		children: Snippet;
+		isSecondary?: boolean;
+		isDanger?: boolean;
+		isMenu?: boolean;
+	}
+
+	interface ButtonProps extends BasicProps {
+		onclick: (e: MouseEvent) => void;
+		href?: never;
+	}
+
+	interface LinkProps extends BasicProps {
+		href: string;
+		onclick?: never;
+	}
+
+	type ComponentProps = ButtonProps | LinkProps;
+
+	let {
+		// Core functionality
+		children,
+		href,
+		onclick,
+		// Variants
+		isMenu = false,
+		isSecondary = false,
+		isDanger = false,
+		// Rest of props
+		...props
+	}: ComponentProps = $props();
+</script>
+
+{#if href}
+	<a
+		{href}
+		class="btn"
+		class:btn-secondary={isSecondary}
+		class:btn-primary={isMenu}
+		class:btn-danger={isDanger}
+	>
+		{@render children()}
+	</a>
+{:else}
+	<button
+		{onclick}
+		{...props}
+		class="btn"
+		class:btn-secondary={isSecondary}
+		class:btn-primary={isMenu}
+		class:btn-danger={isDanger}
+	>
+		{@render children()}
+	</button>
+{/if}
+
+<style>
+	a {
+		display: block;
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: none;
+	}
+
+	.btn {
+		padding: 12px 24px;
+		min-width: 230px;
+		text-align: center;
+		background-color: black;
+		border-radius: 12px;
+		color: white;
+		border: 1px solid white;
+		font-weight: normal;
+		font-size: 22px;
+	}
+
+	.btn-secondary {
+		background-color: white;
+		color: black;
+		border: 1px solid black;
+	}
+
+	.btn-danger {
+		background-color: rgb(136, 4, 4);
+	}
+
+	.btn-menu {
+		min-width: 150px;
+		padding: 8px 20px;
+	}
+</style>
