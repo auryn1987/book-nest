b76c41266b9906377c4344aaa0839b8c835e813c
Benjamin Curtain <benjamin.curtain@gmail.com>
2024-11-11 22:24:38 +1100

    Add header and hero section

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import heroImage from '$assets/hero.png';
-	import Button from './Button.svelte';
+	import { Button } from '$components';
 </script>
 
 <section class="default-margin hero">
---
+++ src/lib/components/Layout/Header.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import bookNestLogo from '$assets/app-logo.svg';
+	import { Button } from '$components';
+</script>
+
+<header>
+	<a href="/">
+		<img class="logo" src={bookNestLogo} alt="Book Nest Logo" />
+	</a>
+	<nav>
+		<ul>
+			<li>
+				<Button isMenu={true} href="/register">Create Account</Button>
+			</li>
+			<li>
+				<Button isMenu={true} isSecondary={true} href="/login">Login</Button>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 12px 4vw;
+	}
+
+	ul {
+		display: flex;
+		column-gap: 24px;
+	}
+
+	.logo {
+		height: 72px;
+	}
+</style>
