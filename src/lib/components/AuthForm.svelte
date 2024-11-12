97406a49a6a72a23408d796ef391c98a45aa8366
Benjamin Curtain <benjamin.curtain@gmail.com>
2024-11-13 07:51:38 +1100

    Add auth form and routes

---
+++ src/lib/components/AuthForm.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import { Button } from '$components';
+
+	interface ComponentProps {
+		isRegistration: boolean;
+	}
+
+	let { isRegistration }: ComponentProps = $props();
+</script>
+
+<div class="default-margin auth-container">
+	<h1 class="mb-l">{isRegistration ? 'Register' : 'Login'}</h1>
+	<div class="form-and-social-login">
+		<form class="auth-form">
+			{#if isRegistration}
+				<input placeholder="Name" type="text" name="name" />
+			{/if}
+			<input placeholder="Email" type="text" name="email" />
+			<input placeholder="Password" type="password" name="password" />
+			{#if isRegistration}
+				<input placeholder="Confirm Password" type="password" name="passwordConfirmation" />
+			{/if}
+			<Button type="submit">{isRegistration ? 'Register' : 'Login'}</Button>
+			{#if isRegistration}
+				<p class="auth-hint mt-s">Already have an account? <a href="/login">Login</a></p>
+			{:else}
+				<p class="auth-hint mt-s">Don't have an account? <a href="/register">Register</a></p>
+			{/if}
+		</form>
+		<div class="social-login"></div>
+	</div>
+</div>
+
+<style>
+	.auth-container {
+		margin-top: 80px;
+	}
+
+	.form-and-social-login {
+		display: flex;
+	}
+
+	.auth-form {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		border-right: 1px solid grey;
+		padding-right: 80px;
+		width: 40%;
+	}
+
+	.auth-hint {
+		font-size: 16px;
+		color: grey;
+	}
+
+	.auth-hint input:last-of-type {
+		margin-bottom: 16px;
+	}
+
+	.auth-error {
+		background-color: rgb(122, 35, 35);
+		color: white;
+		font-size: 18px;
+		border-radius: 12px;
+		padding: 12px;
+		width: 100%;
+		margin-bottom: 8px;
+	}
+
+	.auth-form input {
+		width: 100%;
+		margin-bottom: 12px;
+	}
+
+	.auth-form input:last-of-type {
+		margin-bottom: 30px;
+	}
+
+	.social-login {
+		padding-left: 80px;
+		width: 40%;
+	}
+</style>
